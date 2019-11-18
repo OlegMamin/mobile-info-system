03362bf8c9b950e52ca2193c6a0ff56d6f30d990
Oleg Mamin <maminoleg19@gmail.com>
2019-11-18 16:06:02 +0300

    Modify StartupListener with spring

--- src/main/java/ru/levelup/junior/web/StartupListener.java
+++ src/main/java/ru/levelup/junior/web/StartupListener.java
@@ -1,5 +1,9 @@
 package ru.levelup.junior.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 import ru.levelup.junior.entities.Option;
 import ru.levelup.junior.entities.Tariff;
 import ru.levelup.junior.dao.ContractsDAO;
@@ -21,38 +25,61 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.util.Date;
+import java.util.Random;
 
 
 /**
  * Created by otherz on 11.11.2019.
  */
-
+@Component
 public class StartupListener {
 
-    public void contextInitialized(ServletContextEvent event) {
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("ProdPersistenceUnit");
-        EntityManager manager = factory.createEntityManager();
+    @Autowired
+    private ClientsDAO clientsDAO;
+
+    @Autowired
+    private ContractsDAO contractsDAO;
+
+    @Autowired
+    private TariffsDAO tariffsDAO;
+
+    @Autowired
+    private OptionsDAO optionsDAO;
+
+    @Autowired
+    private EntityManager manager;
+
+    @EventListener
+    public void handleContextRefreshEvent(ContextRefreshedEvent ctxStartEvt) {
+        Client firstClient;
+        Client secondClient;
+        Tariff tariffLow;
+        Tariff tariffHigh;
+        Tariff tariffMedium;
+        Contract contract1;
+        Contract contract2;
+        Contract contract3;
+        Contract contract4;
+        Option option1;
+        Option option2;
+
         manager.getTransaction().begin();
-
-        ClientsDAO clientsDAO = new ClientsDAO(manager);
-        ContractsDAO contractsDAO = new ContractsDAO(manager);
-        TariffsDAO tariffsDAO = new TariffsDAO(manager);
-        OptionsDAO optionsDAO = new OptionsDAO(manager);
-
         try {
-            clientsDAO.findByLogin("test");
+            firstClient = clientsDAO.findByLogin("test");
+            secondClient = clientsDAO.findByLogin("second");
         } catch (NoResultException notFound) {
-            Client client1 = new Client("John", "Terry", 1234564145, "test", "1234");
-            Client client2 = new Client("Frank", "Lampard", 1005323232, "frog", "4567");
-            Tariff tariffLow = new Tariff("tariffLow", 100);
-            Tariff tariffHigh = new Tariff("tariffHigh", 300);
-            Tariff tariffMedium = new Tariff("tariffMedium", 200);
-            Contract contract1 = new Contract(7557755, client1, tariffLow);
-            Contract contract2 = new Contract(1112233, client1, tariffHigh);
-            Contract contract3 = new Contract(2322212, client1, tariffMedium);
-            Contract contract4 = new Contract(4666666, client2, tariffHigh);
-            Option option1 = new Option("testDao1", 10, 3);
-            Option option2 = new Option("testDao2", 12, 4);
+            firstClient = new Client("John", "Terry", 1234564145, "test", "1234");
+            secondClient = new Client("Frank", "Lampard", 1005323232, "frog", "4567");
+            tariffLow = new Tariff("tariffLow", 100);
+            tariffHigh = new Tariff("tariffHigh", 300);
+            tariffMedium = new Tariff("tariffMedium", 200);
+            contract1 = new Contract(7557755, firstClient, tariffLow);
+            contract2 = new Contract(1112233, firstClient, tariffHigh);
+            contract3 = new Contract(2322212, firstClient, tariffMedium);
+            contract4 = new Contract(4666666, secondClient, tariffHigh);
+            option1 = new Option("testDao1", 10, 3);
+            option2 = new Option("testDao2", 12, 4);
 
             contract1.getOptions().add(option1);
             contract1.getOptions().add(option2);
@@ -61,8 +88,8 @@ public class StartupListener {
             contract3.getOptions().add(option2);
             contract4.getOptions().add(option2);
 
-            clientsDAO.create(client1);
-            clientsDAO.create(client2);
+            clientsDAO.create(firstClient);
+            clientsDAO.create(secondClient);
             tariffsDAO.create(tariffHigh);
             tariffsDAO.create(tariffLow);
             tariffsDAO.create(tariffMedium);
@@ -74,22 +101,6 @@ public class StartupListener {
             optionsDAO.create(option2);
 
             manager.getTransaction().commit();
-        } finally {
-            manager.close();
         }
-        event.getServletContext().setAttribute("factory", factory);
-    }
-
-
-    public void contextDestroyed(ServletContextEvent event) {
-        EntityManagerFactory factory = getFactory(event.getServletContext());
-
-        if (factory != null) {
-            factory.close();
-        }
-    }
-
-    public static EntityManagerFactory getFactory(ServletContext context) {
-        return (EntityManagerFactory) context.getAttribute("factory");
     }
 }
