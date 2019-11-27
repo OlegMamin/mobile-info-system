74ae4a0bd045c14780fd4200f5f4534e1d08b60a
Oleg Mamin <maminoleg19@gmail.com>
2019-11-27 12:42:18 +0300

    Add CharsetEncodingFilter

---
+++ src/main/java/ru/levelup/junior/web/CharsetEncodingFilter.java
@@ -0,0 +1,31 @@
+package ru.levelup.junior.web;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * Created by otherz on 27.11.2019.
+ */
+@WebFilter(urlPatterns = "*")
+public class CharsetEncodingFilter implements Filter{
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
